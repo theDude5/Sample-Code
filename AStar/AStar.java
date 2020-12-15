fd30f6ba17d8dad8bfdde24b91780b8abb7c7922
Stuart Ussher <swu022@gmail.com>
2020-12-15 14:55:39 +1300

    Update AStar.java

--- AStar/AStar.java
+++ AStar/AStar.java
@@ -96,6 +96,7 @@ public class AStar extends JPanel {
     }
 
     public Dimension getDimension(int padding) { return new Dimension(scale*width+padding, scale*height+padding); }
+    
     public static void main(String[] args) throws FileNotFoundException {
         if (args.length == 0){
             System.out.println("Usage: java AStar <filepath>");
